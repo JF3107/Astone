22519c0fd1b0c9544c9c6ee8950c6a4bc7750530
Jay <fanzm1@lenovo.com>
2018-01-17 09:28:43 +0800

    NJS learning addoing

--- 0115_NodeJS/server.js
+++ 0115_NodeJS/server.js
@@ -1,7 +1,23 @@
 var http = require('http');
+var url = require('url');
 
-http.createServer(function(request,response){
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
+// http.createServer(function(request,response){
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+// }).listen(8888);
+
+function start(route) {
+    http.createServer(function(request,response){
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for "+ pathname +" received");
+        route(pathname);
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }).listen(8888);
+
+    console.log("server started");
+}
+
+exports.start = start; //exports!! 而非 export
