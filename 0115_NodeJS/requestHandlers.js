b14e2fe2a570deed9973dd10667ab03ea19b7ef6
Jay <fanzm1@lenovo.com>
2018-01-19 17:56:51 +0800

    OK .0119 +

--- 0115_NodeJS/requestHandlers.js
+++ 0115_NodeJS/requestHandlers.js
@@ -1,16 +1,27 @@
 
+var exec = require("child_process").exec;
+
 function start() {
     console.log("Request handler 'start' was called.");
 
-    function sleep(milliSeconds){
-        var startTime = new Date().getTime();
-        var i = 1;
-        while(new Date().getTime() < startTime + milliSeconds);
-    }
+    // function sleep(milliSeconds){
+    //     var startTime = new Date().getTime();
+    //     var i = 1;
+    //     while(new Date().getTime() < startTime + milliSeconds);
+    // }
 
-    sleep(10000);
+    // sleep(10000);
 
-    return "Hello START";
+    // return "Hello START";
+
+    var content = "empty";
+    
+    exec("ls -la", function(error, stdout, stderr){
+        console.log(stderr)
+        content = stdout;
+    });
+
+    return content;
 }
 
 function upload() {
